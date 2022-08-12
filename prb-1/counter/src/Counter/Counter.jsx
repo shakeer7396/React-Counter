839e5e12d46cbcb54e1d479e42764c9658142133
shakeer7396 <97457588+shakeer7396@users.noreply.github.com>
2022-08-12 23:22:02 +0530

    s

--- prb-1/counter/src/Counter/Counter.jsx
+++ prb-1/counter/src/Counter/Counter.jsx
@@ -4,14 +4,13 @@ const Counter = () => {
     const [counter,setCounter]=useState(0)
 
     const handleInc= () =>{ setCounter(counter+1)}
-    const handleDec= () =>{ setCounter(counter-1)}
 
    
   return (
     <div>
         <h1>COUNT :- {counter}</h1>
         <button style={{backgroundColor:"green"}}onClick={handleInc}>INC</button>
-        <button style={{backgroundColor:"orange"}}onClick={handleDec}>DEC</button>
+        <button style={{backgroundColor:"orange"}}onClick={()=>setCounter(counter-1)}>DEC</button>
     
     </div>
   )
